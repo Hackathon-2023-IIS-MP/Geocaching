8848a3512b7e40e66f4afd140447997f3f891f35
Simone <piccinini.simone2005@gmail.com>
2023-12-17 16:00:49 +0100

    created dynamic navbar and footer with a basic jumbotron in index.php

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Geocaching</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include __DIR__ . "/tmpl/header.html.php";
+?>
+
+<!-- Content specific to the home page -->
+<div class="jumbotron mt-4">
+    <h1 class="display-4">Welcome to Geocaching!</h1>
+    <p class="lead">Discover hidden treasures around the world with Geocaching.</p>
+    <hr class="my-4">
+    <p>Explore and share your adventures.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
+</div>
+
+
+</div><!-- Chiusura container iniziato in header.html.php -->
+
+
+
+<?php
+// FOOTER
+include __DIR__ . "/tmpl/footer.html.php";
+?>
+
+
+<!-- Content container ends -->
+</div>
+
---
+++ tmpl/header.html.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Geocaching</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+    <!-- Bootstrap JS (Bootstrap 5 uses native JavaScript for components) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light border">
+    <div class="container"> <!-- Wrap content in a container -->
+        <a class="navbar-brand" href="#">Geocaching</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Profile</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Content container starts with padding -->
+<div class="container">
+
+
+</body>
+</html>
